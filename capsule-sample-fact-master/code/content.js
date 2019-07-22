1e4a1cad74382c714f8b8135f521e81a00472da6
MelyArias <1187770@g.essextech.org>
2019-07-22 16:07:00 -0400

    Add files via upload

---
+++ capsule-sample-fact-master/code/content.js
@@ -0,0 +1,171 @@
+module.exports = [
+  // Replace content below to customize
+  // tags are optional and used for "find facts about [tag]" etc
+  // image is optional - displayed on UI
+  // START PASTE FROM SPREADSHEET HERE
+  {
+    tags : ["Audrey", "Audrey Hepburn"],
+    text: "Nothing is impossible, the word itself says \“I’m possible\”! Quote by Audrey Hepburn",
+    image: {
+      url: "https://live.staticflickr.com/1594/23845740264_447929218d_z.jpg"
+    },
+    bio: "Audrey Hepburn was a British actress and humanitarian. Recognised as a film and fashion icon, Hepburn was active during Hollywood's Golden Age."
+  },
+  
+{
+    tags : ["Steve", "Steve Jobs"],
+    text: "Have the courage to follow your heart and intuition. They somehow know what you truly want to become. Quote by Steve Jobs",
+    image: {
+      url: "https://thenypost.files.wordpress.com/2013/10/stevejobsbig.jpg"
+    },
+    bio: "Steven Paul Jobs was an American inventor, designer and entrepreneur who was the co-founder, chief executive and chairman of Apple Computer."
+  },
+{
+    tags : ["Maya", "Maya Angelou"],
+    text: "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel. Quote by Maya Angelou",
+    image: {
+      url: "https://live.staticflickr.com/8327/8449738207_7d3c5f83ae_z.jpg"
+     },
+    bio: "Maya Angelou was an American poet, singer, memoirist, and civil rights activist. She published seven autobiographies, three books of essays, several books of poetry, and is credited with a list of plays, movies, and television shows spanning over 50 years."
+  },
+{
+    tags : ["William", "William James"],
+    text: "Act as if what you do makes a difference. It does. Quote by William James",
+    image: {
+      url: "https://cdn.britannica.com/s:290x800/10/10710-004-76C3C341.jpg"
+    },
+    bio: "William James was an American philosopher and psychologist, and the first educator to offer a psychology course in the United States. "
+  },
+{
+    tags : ["Henry", "Henry Ford", "Ford"],
+    text: "Whether you think you can or you think you can’t, you’re right. Quote by Henry Ford",
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/commons/1/18/Henry_ford_1919.jpg"
+     },
+    bio: "Henry Ford was an American industrialist and a business magnate, the founder of the Ford Motor Company, and the sponsor of the development of the assembly line technique of mass production."
+  },
+{
+    tags : ["Albert", "Albert Einstein"],
+    text: "Creativity is contagious, pass it on. Quote by Albert Einstein",
+    image: {
+      url: "https://i0.kym-cdn.com/entries/icons/original/000/015/725/url-3-10tjli1.jpeg"
+    },
+    bio: "Albert Einstein was a German-born theoretical physicist who developed the theory of relativity, one of the two pillars of modern physics."
+  },
+{
+    tags : ["Tony", "Tony Robbins"],
+    text: "If you do what you’ve always done, you’ll get what you’ve always gotten. Quote by Tony Robbins",
+    image: {
+      url: "https://image.cnbcfm.com/api/v1/image/105610227-1544111734855gettyimages-546295772.jpeg?v=1553788337&w=1400&h=950"
+     },
+    bio: "Anthony Jay Robbins is an American author, philanthropist, and life coach"
+  },
+{
+    tags : ["Frank Sinatra",  "Sinatra"],
+    text: "The best revenge is massive success. Quote by Frank Sinatra",
+    image: {
+      url: "https://live.staticflickr.com/2130/1547214740_fbc18fc0f8_z.jpg"
+    },
+    bio: "One of the towering figures of the 20th century, the first teen idol and the definitive saloon singer, the latter exemplified on a series of '50s concept albums."
+  },
+{
+    tags : ["Thomas", "Thomas Edison"],
+    text: "I have not failed, I’ve just found 10,00 ways that won't work. Quote by Thomas Alva Edison",
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Thomas_Edison2.jpg"
+    },
+    bio: "Thomas Alva Edison was an American inventor and businessman, who has been described as America's greatest inventor."
+  },
+{
+    tags : ["Muhammad", "Muhammad Ali"],
+    text: "If my mind can CONCEIVE it, and my heart can BELIEVE it -- then I can ACHIEVE it. Quote by Muhammad Ali",
+    image: {
+      url: "http://static.boxrec.com/thumb/4/43/Ali_480938006.jpg/300px-Ali_480938006.jpg"
+     },
+    bio: "Muhammad Ali was an American professional boxer, activist, and philanthropist."
+  },
+{
+    tags : ["T.S.", "T.S Eliot"],
+    text: "Only those who will risk going too far can possibly find out how far one can go. Quote by T. S. Eliot ",
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Thomas_Stearns_Eliot_by_Lady_Ottoline_Morrell_%281934%29.jpg/220px-Thomas_Stearns_Eliot_by_Lady_Ottoline_Morrell_%281934%29.jpg"
+     },
+    bio: "Thomas Stearns Eliot OM, one of the twentieth century's major poets, was also an essayist, publisher, playwright, and literary and social critic."
+  },
+{
+    tags : ["Malcolm", "Malcolm Forbes"],
+    text: "Failure is success if we learn from it. Quote by Malcolm Forbes",
+    image: {
+      url: "http://assets.nydailynews.com/polopoly_fs/1.1513272.1447257121!/img/httpImage/image.jpg_gen/derivatives/gallery_1200/malcolm-forbes.jpg"
+     },
+    bio: "Malcolm Stevenson Forbes was an American entrepreneur most prominently known as the publisher of Forbes magazine, founded by his father B. C. Forbes."
+  },
+{
+    tags : ["Oprah", "Oprah Winfrey"],
+    text: "Turn your wounds into wisdom. Quote by Oprah Winfrey",
+    image: {
+      url: "https://media.npr.org/assets/img/2015/11/09/oprah-28952243b0f9aff76750c98f780645076fcd47cd-s800-c85.jpg"
+     },
+    bio: "Oprah Gail Winfrey is an American media executive, actress, talk show host, television producer and philanthropist. She is best known for her talk show The Oprah Winfrey Show, which was the highest-rated television program of its kind in history and was nationally syndicated from 1986 to 2011 in Chicago."
+  },
+{
+    tags : ["Michelle", "Michelle Obama"],
+    text: "Success is only meaningful and enjoyable if it feels like your own. Quote by Michelle Obama",
+    image: {
+      url: "https://media.glamour.com/photos/5cb61ab5b079812e17ada647/6:7/w_2670,h_3115,c_limit/michelle-obama.jpg"
+     },
+    bio: "Michelle LaVaughn Obama is an American lawyer, university administrator and writer, who was First Lady of the United States from 2009 to 2017."
+  },
+{
+    tags : ["Michelle", "Michelle Obama"],
+    text: "There are still many causes worth sacrificing for, so much history yet to be made. Quote by Michelle Obama",
+    image: {
+      url: "https://media.glamour.com/photos/5cb61ab5b079812e17ada647/6:7/w_2670,h_3115,c_limit/michelle-obama.jpg"
+    },
+    bio: "Michelle LaVaughn Obama is an American lawyer, university administrator and writer, who was First Lady of the United States from 2009 to 2017."
+  },
+{
+    tags : ["Reagan", "Ronald Reagan"],
+    text: "We can’t help everyone, but everyone can help someone. Quote by Ronald Reagan",
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/commons/c/c7/Ronald_Reagan_presidential_portrait_crop.jpg"
+     },
+    bio: "Ronald Wilson Reagan was an American politician and film actor who served as the 40th president of the United States from 1981 to 1989."
+  },
+{
+    tags : ["Seuss", "Dr.Seuss"],
+    text: "You're on your own. And you know what you know. And YOU are the one who'll decide where to GO… Quote by Dr.Seuss",
+    image: {
+      url: "http://crushingkrisis.com/assets/dr-seuss-ew.jpg"
+     },
+    bio: "Theodor Seuss Geisel was an American author, political cartoonist, poet, animator, book publisher, and artist, best known for authoring more than 60 children's books under the pen name Doctor Seuss."
+  },
+{
+    tags : ["Helen", "Helen Keller"],
+    text: "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence. Quote by Helen Keller",
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Helen_Keller.jpg"
+    },
+    bio: "Helen Adams Keller was an American author, political activist, and lecturer. She was the first deaf-blind person to earn a Bachelor of Arts degree."
+  },
+
+  {
+    tags : ["Helen", "Helen Keller"],
+    text: "Life is a daring adventure or it is nothing at all! Quote by Helen Keller",
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Helen_Keller.jpg"
+    },
+    bio: "Helen Adams Keller was an American author, political activist, and lecturer. She was the first deaf-blind person to earn a Bachelor of Arts degree."
+  },
+
+  {
+    tags : ["Einstein", "Albert Einstein"],
+    text: "Great spirits have always encountered violent opposition from mediocre minds. Quote by Albert Einstein",
+    image: {
+      url: "https://live.staticflickr.com/7429/12637209434_fb2db2bb69_z.jpg"
+     },
+    bio: "Albert Einstein was a German-born theoretical physicist who developed the theory of relativity, one of the two pillars of modern physics. His work is also known for its influence on the philosophy of science." 
+  }
+
+  // END PASTE FROM SPREADSHEET HERE
+]
